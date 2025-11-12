d23c0f7a3fa33941e01c971c9469cf0780063d2a
0001159198-rgb <0001159198@senaimgaluno.com.br>
2025-11-12 10:52:35 -0300

    Refactor desenvolvedor class constructor and methods

--- src/desenvolvedor.java
+++ src/desenvolvedor.java
@@ -1,14 +1,14 @@
 public class desenvolvedor extends funcionario {
-    // Atributo específico
+    
     private int quantidadeProjetos;
 
-    // Construtor
+    
     public desenvolvedor(String nome, double salarioBase, int quantidadeProjetos) {
-        super(nome, salarioBase); // chama o construtor da classe Funcionario
+        super(nome, salarioBase); 
         this.quantidadeProjetos = quantidadeProjetos;
     }
 
-    // Getter e Setter
+    
     public int getQuantidadeProjetos() {
         return quantidadeProjetos;
     }
@@ -17,7 +17,7 @@ public class desenvolvedor extends funcionario {
         this.quantidadeProjetos = quantidadeProjetos;
     }
 
-    // Implementação do método abstrato
+    
     @Override
     public double calcularSalario() {
         return getSalarioBase() + (quantidadeProjetos * 500);
